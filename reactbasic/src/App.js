dcee40268f164ae8af4c4caa235d8b99468dfd87
Sahana-pixel <sahanaacharya85@gmail.com>
2024-11-16 16:11:02 +0530

    create component

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,7 +1,10 @@
 
 function App() {
   return (
-   <h1>react basic</h1>
+   <h1>react basic
+    1.package json : scripts,react dom, react native
+    2.simplified, deleted files
+   </h1>
   );
 }
 
---
+++ vitereact/src/Meow.jsx
@@ -0,0 +1,13 @@
+
+
+const Meow = () => {
+  return (
+    <div>
+      <h1>meow</h1>
+    </div>
+  )
+}
+
+export default Meow
+
+// Meow.jsx first letter should be capital
